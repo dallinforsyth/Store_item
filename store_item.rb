494658f11277be31bcebf19a040e73ff63056f50
dallinforsyth <dallfor192@gmail.com>
2023-01-10 15:25:39 -0600

    creating a class

--- store_item.rb
+++ store_item.rb
@@ -1,4 +1,5 @@
-boat = {:item => "boat", :price => 120000, :color => grey}
-car = {:item => "car", :price => 30000, :color => blue}
-motocycle {:item => "motocycle", :price => 20000, :color => red}
- 
+# boat = { :item => "boat", :price => 120000, :color => "grey" }
+# car = { :item => "car", :price => 30000, :color => "blue" }
+# motocycle = { :item => "motocycle", :price => 20000, :color => "red" }
+
+# puts "#{boat[:item]}"
