399f88f23091680359219614664d004f75c29e20
dallinforsyth <dallfor192@gmail.com>
2023-01-10 13:51:28 -0600

    created 3 vechile hashes

--- store_item.rb
+++ store_item.rb
@@ -1,1 +1,4 @@
-
+boat = {:item => "boat", :price => 120000, :color => grey}
+car = {:item => "car", :price => 30000, :color => blue}
+motocycle {:item => "motocycle", :price => 20000, :color => red}
+ 
